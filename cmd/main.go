71126c818e1a3d629973bdc2a64554f703b15f60
Dhruv Arora <dhruv862arora@gmail.com>
2025-07-26 14:13:28 +0530

    feat: add gorilla-mux

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	r := mux.NewRouter().StrictSlash(true)
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Received method:", r.Method)
+		w.Header().Set("Content-Type", "text/plain")
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Hello, KiteCopyTrader!"))
-	})
+		w.Write([]byte("GET or POST request received successfully!"))
+	}).Methods("POST", "GET")
+
 	log.Println("Server running at http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
